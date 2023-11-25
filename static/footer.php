a94ba12f09b9669d76ee2acdce4023798ad5d88d
HAMZA  EL GHAMCH <114395295+hamza-el-ghamch@users.noreply.github.com>
2023-11-25 13:59:05 +0100

    Update footer.php

--- static/footer.php
+++ static/footer.php
@@ -1,5 +1,5 @@
 <footer>
-	<p>Réalisé par <a href="https://timotheehuneau.fr">Timothée Huneau</a></p>
-	<p>@2022</p>
-	<p>Icons from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></p>
+	<p>Réalisé par www.hamzaelghamch.com <a">Timothée Huneau</a></p>
+	<p>@2023</p>
+	<p>Icons from <a href="" title="Flaticon"> </a></p>
 </footer>
