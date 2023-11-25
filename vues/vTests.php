63fa5e561b06960766fd3277dd1729fecd52a924
HAMZA  EL GHAMCH <114395295+hamza-el-ghamch@users.noreply.github.com>
2023-11-25 14:00:07 +0100

    Update vTests.php

--- vues/vTests.php
+++ vues/vTests.php
@@ -2,6 +2,7 @@
 	<form method="POST" action="index.php?page=test">
 		<input type="text" required name="textTest" value="100" title="<?=DESC_CHAMPS_DUREE?>"/>
 		<input type="submit">
+		
 	</form>
 	<a href="index.php?page=ajouterElement">Retourner à la liste</a>
 </section>
