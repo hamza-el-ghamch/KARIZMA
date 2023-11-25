36e1393b2fee986b4e78c823c0a14e5f19751810
HAMZA  EL GHAMCH <114395295+hamza-el-ghamch@users.noreply.github.com>
2023-11-25 14:03:25 +0100

    Update vSupprimerRecette.php

--- vues/vSupprimerRecette.php
+++ vues/vSupprimerRecette.php
@@ -4,7 +4,8 @@
 La suppresion s'est bien déroulée.</h2>
 <?php else: ?>
 La recette n'a pas pû être supprimée</h2>
-<p>Vérifiez qu'elle existe (encore) et qu'elle n'est utilisée nul part ailleur.</p>
+<p>Vérifiez qu'elle existe (encore) et
+qu'elle n'est utilisée nul part ailleur.</p>
 <?php endif ?>
 <p>Vous pouvez retourner à <a href="index.php">l'accueil</a> ou à <a href="index.php?page=ajouterElement" >la liste des ingrédients</a></p>
 </section>
